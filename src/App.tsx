7df21b5a9b153071f774bef0e49971319bbad495
F0oocuS <ihoryaremko@gmail.com>
2025-08-20 13:28:18 +0300

    add typescrit and .prettierrc.json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App(): React.ReactElement {
+	return (
+        <div className="card">
+	        Hello world
+        </div>
+	)
+}
+
+export default App
