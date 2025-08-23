2042e90ef755888d118d8ab6edc64fc2aa02a8ab
F0oocuS <ihoryaremko@gmail.com>
2025-08-23 17:23:27 +0300

    download leaflet and some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+
+import Text from './components/test/Text.tsx';
+
+import './App.scss';
+
+import leftArrow from '../src/assets/icons/angle-left-icon.svg';
+import rightArrow from '../src/assets/icons/angle-right-icon.svg';
 
 function App(): React.ReactElement {
-	return <div className="card">Hello world</div>;
+	const [isLeftSidebarOpen, setIsLeftSidebarOpen] = useState(false);
+	const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
+
+	const handleToggleLeftSidebar = () => {
+		setIsLeftSidebarOpen(!isLeftSidebarOpen);
+	};
+
+	const handleToggleRightSidebar = () => {
+		setIsRightSidebarOpen(!isRightSidebarOpen);
+	};
+	return (
+		<div className="app">
+			<div className="app__container">
+				<div className={`app__sidebar ${isLeftSidebarOpen ? 'app__sidebar--open' : ''}`}>
+					<Text />
+
+					<button className="app__sidebar-toggler" onClick={handleToggleLeftSidebar}>
+						<img src={ rightArrow } alt="Right arrow" className="app__toggler-icon"/>
+					</button>
+				</div>
+
+				<div className="app__content">
+					<MapContainer
+						center={[50.4501, 30.5234]} // центр Києва
+						zoom={12}
+						style={{ height: "100vh", width: "100%" }}
+					>
+						<TileLayer
+							url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+							minZoom={10}
+							maxZoom={16}
+							attribution="&copy; Local Tiles"
+						/>
+					</MapContainer>
+				</div>
+
+				<div className={`app__sidebar app__sidebar--right ${isRightSidebarOpen ? 'app__sidebar--open' : ''}`}>
+					<Text />
+
+					<button className="app__sidebar-toggler app__sidebar-toggler--right" onClick={handleToggleRightSidebar}>
+						<img src={ leftArrow } alt="Left arrow" className="app__toggler-icon"/>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/test/Text.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Text.scss';
+
+function Text(): React.ReactElement {
+	return <p className="text">Hello World!</p>;
+}
+
+export default Text;
